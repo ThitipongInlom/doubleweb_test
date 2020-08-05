b626a4c427a4df33fa2642a743e9f88bd98bc9d6
ThitipongInlom <ingnice007@gmail.com>
2020-08-05 17:15:04 +0700

    แก้ไข เพิ่มช่องทางการ Delete

--- api/blacklist_member/delete.php
+++ api/blacklist_member/delete.php
@@ -68,6 +68,102 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
                 "message" => "ไม่มี Token"
             ));
         }
+    }else if (!empty($data->bank_number)) {
+        if (isset($all_headers["Authorization"])) {
+
+            $jwt = $all_headers['Authorization'];
+
+            if (!empty($jwt)) {
+                try {
+
+                    $secret_key = "owt125";
+
+                    $decoded_data = JWT::decode($jwt, $secret_key, array('HS512'));
+
+                    $user_id = $decoded_data->data->id;
+
+                    $mysqli_delete = "UPDATE blacklist_member SET
+                        is_delete = '1'
+                        WHERE bank_number = $data->bank_number
+                    ";
+
+                    $conn->query($mysqli_delete);
+                    
+                    http_response_code(200);
+                    echo json_encode(array(
+                        "status" => 1,
+                        "message" => 'บันทึก เสร็จสิ้น'
+                    ));
+                } catch (Exception $ex) {
+
+                    http_response_code(500);
+                    echo json_encode(array(
+                        "status" => 0,
+                        "message" =>  $ex->getMessage()
+                    ));
+                }
+            }else {
+                http_response_code(404);
+                echo json_encode(array(
+                    "status" => 0,
+                    "message" => "ไม่มี Token"
+                ));   
+            }
+        } else {
+            http_response_code(404);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "ไม่มี Token"
+            ));
+        }
+    }else if (!empty($data->phone_number)) {
+        if (isset($all_headers["Authorization"])) {
+
+            $jwt = $all_headers['Authorization'];
+
+            if (!empty($jwt)) {
+                try {
+
+                    $secret_key = "owt125";
+
+                    $decoded_data = JWT::decode($jwt, $secret_key, array('HS512'));
+
+                    $user_id = $decoded_data->data->id;
+
+                    $mysqli_delete = "UPDATE blacklist_member SET
+                        is_delete = '1'
+                        WHERE phone_number = $data->phone_number
+                    ";
+
+                    $conn->query($mysqli_delete);
+                    
+                    http_response_code(200);
+                    echo json_encode(array(
+                        "status" => 1,
+                        "message" => 'บันทึก เสร็จสิ้น'
+                    ));
+                } catch (Exception $ex) {
+
+                    http_response_code(500);
+                    echo json_encode(array(
+                        "status" => 0,
+                        "message" =>  $ex->getMessage()
+                    ));
+                }
+            }else {
+                http_response_code(404);
+                echo json_encode(array(
+                    "status" => 0,
+                    "message" => "ไม่มี Token"
+                ));   
+            }
+        } else {
+            http_response_code(404);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "ไม่มี Token"
+            ));
+        }
     } else {
         http_response_code(404);
         echo json_encode(array(
--- api/blacklist_member/search.php
+++ api/blacklist_member/search.php
@@ -20,21 +20,9 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
     // head
     $all_headers = getallheaders();
 
-    if (!empty($data->phone_number) OR !empty($data->bank_number)) {
-        // Phone Number
-        if (!empty($data->phone_number)) {
-            $phone_number = "is_delete != '1' AND  phone_number = '$data->phone_number'";
-        }else {
-            $phone_number = "";
-        }
-        // Bank Number
-        if (!empty($data->bank_number)) {
-            $bank_number  = "OR is_delete != '1' AND bank_number = '$data->bank_number'";
-        }else {
-            $bank_number  = "";
-        }
+    if (!empty($data->phone_number)) {
         // Query
-        $sql_seacrh = "SELECT * FROM blacklist_member WHERE $phone_number $bank_number";
+        $sql_seacrh = "SELECT * FROM blacklist_member WHERE is_delete != '1' AND  phone_number = '$data->phone_number'";
 
         if ($result = $conn->query($sql_seacrh)) {
             while ($obj = $result->fetch_object()) {
@@ -61,6 +49,35 @@ if($_SERVER['REQUEST_METHOD'] === "POST") {
                 "message" => "None"
             ));
         }  
+    } else if (!empty($data->bank_number)) {
+        // Query
+        $sql_seacrh = "SELECT * FROM blacklist_member WHERE is_delete != '1' AND  bank_number = '$data->bank_number'";
+
+        if ($result = $conn->query($sql_seacrh)) {
+            while ($obj = $result->fetch_object()) {
+                $fristname   = $obj->fristname;
+                $lastname = $obj->lastname;
+                $phone_number = $obj->phone_number;
+                $bank_number = $obj->bank_number;
+                $block_from = $obj->block_from;
+
+                $check_id = $obj->log_id;
+            }
+            $result->free_result();
+        }
+        if (!empty($check_id)) {
+            http_response_code(200);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "Block"
+            ));
+        }else {
+            http_response_code(200);
+            echo json_encode(array(
+                "status" => 0,
+                "message" => "None"
+            ));
+        } 
     }else {
         http_response_code(404);
         echo json_encode(array(
